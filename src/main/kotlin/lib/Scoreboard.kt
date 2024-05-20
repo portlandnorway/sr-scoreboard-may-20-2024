f2bc25950fb900e2f1a2e877398348fd6f79e5d7
portlandnorway <dennis.neely22@gmail.com>
2024-05-20 17:52:01 +0200

    barebones functions + unit tests that should all fail

--- src/main/kotlin/lib/Scoreboard.kt
+++ src/main/kotlin/lib/Scoreboard.kt
@@ -1,5 +1,24 @@
 package lib
 
-class Scoreboard {
+import lib.model.Match
+import java.util.*
 
+class Scoreboard {
+  private val matches = mutableMapOf<UUID, Match>()
+
+  fun startMatch(home: String, away: String): UUID {
+
+  }
+
+  fun updateScore(matchId: UUID, homeScore: Int, awayScore: Int) {
+
+  }
+
+  fun finishMatch(matchId: UUID) {
+
+  }
+
+  fun getScoreboard(): List<Match> {
+
+  }
 }
